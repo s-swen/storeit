404d2829f7d94a49e5ae967b9578c59f27483066
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2026-02-22 16:30:23 +0000

    pending otp verification

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -2,7 +2,7 @@
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import {Input} from '@/components/ui/input';
+import { Input } from '@/components/ui/input';
 import { Button } from './ui/button';
 import {
   Form,
@@ -13,6 +13,9 @@ import {
   FormLabel,
   FormMessage,
 } from './ui/form';
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
@@ -21,6 +24,8 @@ const formSchema = z.object({
 type FormType = 'sign-in' | 'sign-up';
 
 const AuthForm = ({ type }: { type: FormType }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, seterrorMessage] = useState('')
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -29,26 +34,69 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     console.log(values);
-  }
+  };
   return (
     <>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className='auth-form'>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="form-title">{type === 'sign-in' ? 'Sign In' : 'Sign Up'}</h1>
+          {type === 'sign-up' && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">Full Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your full name" className="shad-input" {...field} />
+                    </FormControl>
+                  </div>
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
+          )}
           <FormField
             control={form.control}
-            name='username'
-            render={({field}) => (
+            name="email"
+            render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder='shadcn' {...field} />
-                </FormControl>
-                <FormDescription>This is your public display name</FormDescription>
-                <FormMessage />
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your email" className="shad-input" {...field} />
+                  </FormControl>
+                </div>
+                <FormMessage className="shad-form-message" />
               </FormItem>
             )}
           />
-          <Button type='submit'>Submit</Button>
+          <Button type="submit" className="form-submit-button">
+            {type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+            {isLoading && (
+              <Image
+                src="/assets/icons/loader.svg"
+                alt="loader"
+                width={24}
+                height={24}
+                className="ml-2 animate-spin"
+              />
+            )}
+          </Button>
+          {errorMessage && (
+            <p className='error-message'>*{errorMessage}</p>
+          )}
+          <div className='body-2 flex justify-center'>
+            <p className='text-light-100'>
+              {type === 'sign-in'
+               ? `Don't have an account?`
+               : 'Already have an account?'
+              }
+            </p>
+            <Link
+              href={type === 'sign-in' ? }
+          </div>
         </form>
       </Form>
       OTP Verification
