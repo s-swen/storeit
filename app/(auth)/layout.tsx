47d2d7491b72ee29568c8af0a6815d333cf8d5c2
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2026-02-22 09:08:18 +0000

    auth form impl

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
-const Layout = ({children}: {children: React.ReactNode}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex min-h-screen'>
-        <section>
-            <div>
-                <Image src='/favicon.ico' alt='logo' width={16} height={16} className='h-auto' />
-            </div>
-        </section>
+    <div className="flex min-h-screen">
+      <section className="bg-brand p-10 items-center justify-center hidden w-1/2 lg:flex xl:w-2/5">
+        <div className='flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12'>
+          <Image src="/assets/icons/logo-full.svg" alt="logo" width={224} height={82} className="h-auto" />
+          <div className="space-y-5 text-white">
+            <h1 className="h1">Manage your files the best way</h1>
+            <p className="body-1">This is a place where you can store all your documents</p>
+          </div>
+          <Image src='/assets/images/files.png' alt='files' width={342} height={342} className='transition-all hover:rotate-2 hover:scale-105'/>
+        </div>
+      </section>
+      <section className='flex flex-col flex-1 items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0'>
+        <div className='mb-16 lg:hidden'>
+          <Image 
+            src='/assets/icons/logo-full-brand.svg'
+            alt='logo'
+            width={224}
+            height={82}
+            className='h-auto w-[200px] lg:w-[250px]'
+          />
+        </div>
         {children}
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
